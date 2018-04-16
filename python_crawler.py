b58b1fcbd267c273871565d6e7dd53e131611aa7
nkaur472 <kaur.navpreet472@gmail.com>
2018-04-16 09:36:17 -0700

    code updated with GUI

--- python_crawler.py
+++ python_crawler.py
@@ -1,13 +1,18 @@
+#Code for Crawler:
+
 import httplib
 import re
 import argparse
 import ttk
 from Tkinter import *
 from ttk import *
-
+import ScrolledText
+processed = [] 
 def open_GUI():
+	global textPad,root
 	root= Tk()
 	root.title("Crawl UsedVictoria")
+	
 
 	mainframe = ttk.Frame(root, padding="3 3 12 12")
 	mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -17,6 +22,10 @@ def open_GUI():
 	url = StringVar()
 	depth = StringVar()
 	search = StringVar()
+	ttk.Label(mainframe, text="Enter URL here").grid(column=1, row=1, sticky=(W,E))
+	ttk.Label(mainframe, text="enter depth for crawler").grid(column=1, row=2, sticky=(W))
+	ttk.Label(mainframe, text="enter serach").grid(column=1, row=3, sticky=(W))
+
 	    
 	url_entry = ttk.Entry(mainframe, width=7, textvariable=url)
 	url_entry.grid(column=2, row=1, sticky=(W,E))
@@ -24,7 +33,7 @@ def open_GUI():
 	depth_entry.grid(column=2, row=2, sticky=(W,E))
 	search_entry = ttk.Entry(mainframe, width=7, textvariable=search)
 	search_entry.grid(column=2, row=3, sticky=(W,E))
-
+	
 
 	ttk.Label(mainframe, textvariable=url).grid(column=5, row=1, sticky=(W,E))
 	ttk.Label(mainframe, textvariable=depth).grid(column=5, row=2, sticky=(W))
@@ -35,57 +44,80 @@ def open_GUI():
 	url_entry.focus()
 	depth_entry.focus()
 	search_entry.focus()
-	ttk.Button(mainframe, text="searchURL", command=lambda  : searchURL(url.get(),int(depth.get()),search.get())).grid(column=3, row=4, sticky=W)
-    
+	textPad = ScrolledText.ScrolledText(mainframe, width=100, height=20)
+	textPad.grid(column=1, row=8, sticky=(W))
+	
+	
+	
+	ttk.Button(mainframe, text="searchURL", command=lambda  : MonitorURL(url.get(),int(depth.get()),search.get())).grid(column=2, row=5, sticky=W)
+	
+	
 	root.mainloop()
 	
 	
-def searchURL(url1,depth1,search1):
-	print(url1)
-	print(depth1)
-	print(search1)
-	processed = []
-    # only do http links
-
-	if url1.startswith("http://") and (not url1 in processed):
-	        processed.append(url1)
-	        url1 = url1.replace("http://", "", 1)
-	        
-	        # split out the url into host and doc
-	        host, path = url1, "/"
-
-	        urlparts = url1.split("/")
-	        if len(urlparts) > 1:
-	                host = urlparts[0]
-	                path = url1.replace(host, "", 1)
-
-	        # make the first request
-	        print "crawling host: " + host + " path: " + path
-	        conn = httplib.HTTPConnection(host)
-	        req = conn.request("GET", path)
-	        res = conn.getresponse()
-
-	        # find the links
-	        contents = res.read()
-	        m = re.findall('href="(/classifieds+.*?)"', contents)
-	        
-	        if search1 in contents:
-	                print "Found " + search1 + " at " + url1
-
-	        print str(depth1) + ": processing " + str(len(m)) + " links"
-	        for href in m:
-	                # do relative urls
-	                if href.startswith("/"):
-	                        href = "http://" + host + href
-
-	                # follow the links
-	                if depth1:
-	                        searchURL(href, depth1-1, search1)
+def MonitorURL(url1,depth1,search1):
+	if url1.startswith("http://"):
+		analyzeURL(url1,depth1,search1)
 	else:
-	        print "skipping " + url1
-
+		textPad.insert(END,"skipping"+url1+"\n")
+		
+def analyzeURL(url1,depth1,search1):
 	
+	if (not url1 in processed):
+		processed.append(url1)
+		url1 = url1.replace("http://", "", 1)
+		host, path = url1, "/"
+
+		urlparts = url1.split("/")
+		if len(urlparts) > 1:
+			host = urlparts[0]
+			path = url1.replace(host, "", 1)
+			planer(host,path,url1,depth1,search1)
+		else:
+			planer(host,path,url1,depth1,search1)
+			
+
+def planer(host,path,url1,depth1,search1):
+	
+	flag=0
+	href = "http://"   
+	# make the first request
+	conn = httplib.HTTPConnection(host)
+	req = conn.request("GET", path)
+	res = conn.getresponse()
+	
+	# find the links in classifieds only
+	contents = res.read()
+	m = re.findall('href="(/classifieds+.*?)"', contents)
+	
+	
+    # looking for the word to be searched
+	if search1 in contents:
+		flag=1
+		execute(search1,url1,flag,href,depth1)
+		
+    #Check for following links
+   	for href in m:
+	        if href.startswith("/"):
+	                href = "http://" + host + href
+	                flag=2
+	                execute(search1,url1,flag,href,depth1)
+
+	        
+def execute(search1,url1,flag,href,depth1):
+	if(flag==1):
+		textPad.insert(END,"Found " + search1 + " at " + url1+ "\n")
+		root.update()
+	# follow the links
+	if (flag==2):
+		if depth1:
+			MonitorURL(href, depth1-1, search1)
 	
 if __name__ == "__main__":
 	open_GUI()
-                        
+                  
+
+#CSC586A-Navpreet-Kaur-V00823334
+#CSC586A-Parminder-Kaur-V00820508
+#CSC586A-Simar-Arora-V00824821
+     
